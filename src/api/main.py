e952edc73d08365c7594cc910938d87e07175209
Yash Jain <yashjain1394@gmail.com>
2025-12-06 16:51:41 +0530

    add fastapi instrumentation

--- src/api/main.py
+++ src/api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from inference import predict_price, batch_predict
 from schemas import HousePredictionRequest, PredictionResponse
+from prometheus_fastapi_instrumentator import Instrumentator
 
 # Initialize FastAPI app with metadata
 app = FastAPI(
@@ -31,6 +32,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Initialize and instrument Prometheus metrics
+Instrumentator().instrument(app).expose(app)
 
 # Health check endpoint
 @app.get("/health", response_model=dict)
